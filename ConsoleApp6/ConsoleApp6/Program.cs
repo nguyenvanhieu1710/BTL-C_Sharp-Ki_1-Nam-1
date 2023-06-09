65690b90a1d5fd683173fa213207500af7e0081a
Nguyen Van Hieu <hieunghiendo1@gmail.com>
2023-06-09 20:11:23 +0700

    giao_dien

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,134 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("╔══════════════════════════════════════╗");
+Console.WriteLine("║            QUẢN LÝ THÔNG TIN LỚP              ║");
+Console.WriteLine("╠══════════════════════════════════════╣");
+Console.WriteLine("║ 1. Thêm sinh viên vào lớp                         ║");
+Console.WriteLine("║ 2. Xoá sinh viên trong lớp                            ║");
+Console.WriteLine("║ 3. Hiển thị thông tin sinh viên                      ║");
+Console.WriteLine("║         trong lớp                                            ║");
+Console.WriteLine("║ 4. Thoát khỏi chương trình                             ║");
+Console.WriteLine("╠══════════════════════════════════════╣");
+Console.WriteLine("║ Hãy chọn một trong các chức năng trên:  ║");
+Console.WriteLine("╚══════════════════════════════════════╝");
+
+Console.WriteLine("╔══════════════════════════════════════╗");
+Console.WriteLine("║            QUẢN LÝ THÔNG TIN LỚP              ║");
+Console.WriteLine("╠══════════════════════════════════════╣");
+Console.WriteLine("║ 1. Thêm sinh viên vào lớp                         ║");
+Console.WriteLine("║     ╚═══════════════════════════════╝");
+Console.WriteLine("║ 2. Xoá sinh viên trong lớp                            ║");
+Console.WriteLine("║     ╚═══════════════════════════════╝");
+Console.WriteLine("║ 3. Hiển thị thông tin sinh viên                      ║");
+Console.WriteLine("║         trong lớp                                            ║");
+Console.WriteLine("║     ╚═══════════════════════════════╝");
+Console.WriteLine("║ 4. Thoát khỏi chương trình                             ║");
+Console.WriteLine("╠══════════════════════════════════════╣");
+Console.WriteLine("║ Hãy chọn một trong các chức năng trên:  ║");
+Console.WriteLine("╚══════════════════════════════════════╝");
+/*
+╔══════════════════════════════════════╗
+║            QUẢN LÝ THÔNG TIN LỚP              ║
+╠══════════════════════════════════════╣
+║ 1. Thêm sinh viên vào lớp                         ║
+║     ╚═══════════════════════════════╝
+║ 2. Xoá sinh viên trong lớp                            ║
+║     ╚═══════════════════════════════╝
+║ 3. Hiển thị thông tin sinh viên                      ║
+║         trong lớp                                            ║
+║     ╚═══════════════════════════════╝
+║ 4. Thoát khỏi chương trình                             ║
+╠══════════════════════════════════════╣
+║ Hãy chọn một trong các chức năng trên:  ║
+╚══════════════════════════════════════╝
+*/
+Console.WriteLine("╔══════════════════════════════════════════════╗");
+Console.WriteLine("║           QUẢN LÝ THÔNG TIN LỚP              ║");
+Console.WriteLine("╠══════════════════════════════════════════════╣");
+Console.WriteLine("║     1. Thêm sinh viên vào lớp                ║");
+Console.WriteLine("║         └─────────────────┘                  ║");
+Console.WriteLine("║     2. Xoá sinh viên trong lớp               ║");
+Console.WriteLine("║         └──────────────────┘                 ║");
+Console.WriteLine("║     3. Hiển thị thông tin sinh viên          ║");
+Console.WriteLine("║         trong lớp                            ║");
+Console.WriteLine("║         └─────────────────┘                  ║");
+Console.WriteLine("║     4. Thoát khỏi chương trình               ║");
+Console.WriteLine("║         └──────────────────┘                 ║");
+Console.WriteLine("╠══════════════════════════════════════════════╣");
+Console.WriteLine("║     Hãy chọn một trong các chức năng trên:   ║");
+Console.WriteLine("╚══════════════════════════════════════════════╝");
+/*
+╔══════════════════════════════════════╗
+║           QUẢN LÝ THÔNG TIN LỚP               ║
+╠══════════════════════════════════════╣
+║     1. Thêm sinh viên vào lớp            ║
+║         └─────────────────┘                      ║
+║     2. Xoá sinh viên trong lớp               ║
+║         └──────────────────┘                     ║
+║     3. Hiển thị thông tin sinh viên        ║
+║         trong lớp                                           ║
+║         └─────────────────┘                       ║
+║     4. Thoát khỏi chương trình              ║
+║         └──────────────────┘                      ║
+╠══════════════════════════════════════╣
+║       Hãy chọn một trong các chức năng trên:   ║
+╚══════════════════════════════════════╝
+*/
+
+Console.WriteLine("╔════════════════════════════════════════════╗");
+Console.WriteLine("║      QUẢN LÝ THÔNG TIN KHOA ĐÀO TẠO        ║");
+Console.WriteLine("╠════════════════════════════════════════════╣");
+Console.WriteLine("║  1. THÊM KHOA ĐÀO TẠO                      ║");
+Console.WriteLine("║         └────────────┘                     ║");
+Console.WriteLine("║  2. HIỂN THỊ CÁC KHOA ĐÀO TẠO ĐÃ CÓ TRONG  ║");
+Console.WriteLine("║     DANH SÁCH                              ║");
+Console.WriteLine("║         └────────────┘                     ║");
+Console.WriteLine("║  3. XEM THÔNG TIN DANH SÁCH LỚP CỦA KHOA   ║");
+Console.WriteLine("║     ĐANG ĐÀO TẠO                           ║");
+Console.WriteLine("║         └────────────┘                     ║");
+Console.WriteLine("║  4. XOÁ KHOA ĐÀO TẠO                       ║");
+Console.WriteLine("║         └────────────┘                     ║");
+Console.WriteLine("║  5. THOÁT KHỎI CHƯƠNG TRÌNH                ║");
+Console.WriteLine("║         └────────────┘                     ║");
+Console.WriteLine("╠════════════════════════════════════════════╣");
+Console.WriteLine("║  Hãy chọn một trong các chức năng trên:    ║");
+Console.WriteLine("╚════════════════════════════════════════════╝");
+
+Console.WriteLine("╔══════════════════════════════════════════════╗");
+Console.WriteLine("║        QUẢN LÝ THÔNG TIN NIÊN KHOA            ║");
+Console.WriteLine("╠══════════════════════════════════════════════╣");
+Console.WriteLine("║     1. THÊM NIÊN KHOÁ                         ║");
+Console.WriteLine("║         └──────────┘                         ║");
+Console.WriteLine("║     2. HIỂN THỊ CÁC NIÊN KHOÁ ĐÃ CÓ TRONG     ║");
+Console.WriteLine("║         DANH SÁCH                             ║");
+Console.WriteLine("║         └──────────────────────┘             ║");
+Console.WriteLine("║     3. XEM THÔNG TIN DANH SÁCH LỚP CỦA NIÊN   ║");
+Console.WriteLine("║         KHOÁ                                  ║");
+Console.WriteLine("║         └────────────────────┘               ║");
+Console.WriteLine("║     4. XOÁ NIÊN KHOÁ                          ║");
+Console.WriteLine("║         └──────────┘                         ║");
+Console.WriteLine("║     5. THOÁT KHỎI CHƯƠNG TRÌNH               ║");
+Console.WriteLine("║         └───────────────┘                    ║");
+Console.WriteLine("╠══════════════════════════════════════════════╣");
+Console.WriteLine("║     Hãy chọn một trong các chức năng trên:   ║");
+Console.WriteLine("╚══════════════════════════════════════════════╝");
+
+Console.WriteLine("╔═════════════════════════════════════════════════╗");
+Console.WriteLine("║           ỨNG DỤNG QUẢN LÝ SINH VIÊN            ║");
+Console.WriteLine("╠═════════════════════════════════════════════════╣");
+Console.WriteLine("║             1. QUẢN LÝ NIÊN KHOÁ                ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("║           2. QUẢN LÝ KHOA ĐÀO TẠO               ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("║             3. QUẢN LÝ THÔNG TIN LỚP            ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("║                4. THÊM LỚP                      ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("║           5. HIỂN THỊ CÁC LỚP ĐANG CÓ           ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("║            6. THOÁT KHỎI CHƯƠNG TRÌNH           ║");
+Console.WriteLine("║         └───────────────────────┘               ║");
+Console.WriteLine("╠═════════════════════════════════════════════════╣");
+Console.WriteLine("║         Hãy chọn một trong các chức năng trên:  ║");
+Console.WriteLine("╚═════════════════════════════════════════════════╝");
+
